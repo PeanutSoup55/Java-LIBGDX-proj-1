b07536b320c3a45a46256efe68759f35909e5bab
Eric Blanchette <ericblanchette55hockey@gmail.com>
2026-02-07 14:45:29 -0500

    added a menu class

--- core/src/main/java/io/github/some_example_name/MainMenuScreen.java
+++ core/src/main/java/io/github/some_example_name/MainMenuScreen.java
@@ -1,4 +1,33 @@
 package io.github.some_example_name;
 
-public class MainMenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainMenuScreen implements Screen {
+    final Drop game;
+
+    public MainMenuScreen(final Drop game){
+        this.game = game;
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
+
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+
+        game.batch.begin();
+        //draw text. Remember that x and y are in meters
+        game.font.draw(game.batch, "Welcome to Drop!!! ", 1, 1.5f);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 1, 1);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
 }
